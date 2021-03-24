8b2c590f2538a96460d036a023b6a925ee8b0aad
Charles Wahome <thewahome.cw@gmail.com>
2021-03-24 15:14:58 +0300

    add validation logic

--- api/ReceiveUpdate/index.ts
+++ api/ReceiveUpdate/index.ts
@@ -1,17 +1,30 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    context.log('Executing webhook endpoint.');
+    if (req.query.validationToken) {
+        context.log('Validating new subscription');
+        context.log('Validating token');
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-
+        context.res = {
+            headers: {
+                'Content-Type': 'text/plain'
+            },
+            body: req.query.validationToken
+        };
+    }
+    else {
+        context.log('Received new notification');
+        context.log(JSON.stringify(req.body));
+        const name = (req.query.name || (req.body && req.body.name));
+        let responseMessage = name
+            ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+            : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: responseMessage
+        };
+    }
 };
 
 export default httpTrigger;
