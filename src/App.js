1b3421e7308c95b24b70ea7af1fd7da1ea3ba07d
Charles Wahome <thewahome.cw@gmail.com>
2021-03-24 14:28:23 +0300

    add frontend files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => (
+  <div className="content-container">
+    <div className="content-title-group not-found">
+      <h2 className="title">Hello World</h2>
+      <p>
+        lorem ipsum
+      </p>
+      <br />
+    </div>
+  </div>
+);
+
+export default About;
